bc8ffccf19481bfa2bcddb5467b9a1eb7ab29f02
Ismael Mori <gold.dev88@gmail.com>
2017-06-16 12:56:53 -0300

    Add index.html, index.js

---
+++ react-booking-form/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { fromJS } from 'immutable';
+import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import Router from 'setup/Router';
+
+injectTapEventPlugin();
+
+function renderApp(Component) {
+  render(
+    <AppContainer>
+      <MuiThemeProvider>
+        <Provider>
+          <Component />
+        </Provider>
+      </MuiThemeProvider>
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+}
+
+renderApp(Router);
+
+if (module.hot) {
+  module.hot.accept('./setup/Router/index.js', () => {
+    const NewApp = require('./setup/Router/index.js').default;
+    renderApp(NewApp);
+  });
+}
