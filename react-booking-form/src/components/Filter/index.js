96af9a6e74a68ea9a857a1d97f343c52ea36986f
Liang Ren <liangren64@gmail.com>
2017-06-22 17:01:35 -0300

    refactor codebase for filter component

--- react-booking-form/src/components/Filter/index.js
+++ react-booking-form/src/components/Filter/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
@@ -7,41 +7,67 @@ import LocationFilter from './LocationFilter';
 import colors from 'styles/colors';
 import styles from './styles';
 
+class Filter extends Component {
+  constructor(props) {
+    super(props);
 
-const Date = [
-  { id: 1, label: <p>start date: </p>, setting: <DatePicker hintText="" textFieldStyle={styles.customDate} /> },
-  { id: 2, label: <p>end date: </p>, setting: <DatePicker hintText="" textFieldStyle={styles.customDate} /> },
-  { id: 3, label: <p>Locations: </p>, setting: <LocationFilter /> },
-];
-function Filter() {
-  return (
-    <div>
-      <div style={{display: 'flex'}}>
-        <h2> Filter </h2>
-        <span style={{marginLeft: 10, marginTop: 25,}}>(to show only properties that match criteria)</span>
+    const startDate = new Date();
+    const endDate = new Date();
+    startDate.setDate(startDate.getDate());
+    startDate.setHours(0,0,0,0);
+    endDate.setDate(endDate.getDate() + 14);
+    endDate.setHours(0,0,0,0);
+    this.state = {
+      startDate: startDate,
+      endDate: endDate,
+    };
+  }
+  handleChangeStartDate = (event, date) => {
+    this.setState({
+      startDate: date,
+    });
+  };
+  handleChangeEndDate = (event, date) => {
+    this.setState({
+      endDate: date,
+    });
+  };
+
+  render() {
+    const Date = [
+      { id: 1, label: <p>start date: </p>, setting: <DatePicker hintText="" defaultDate={this.state.startDate} onChange={this.handleChangeStartDate} textFieldStyle={styles.customDate} /> },
+      { id: 2, label: <p>end date: </p>, setting: <DatePicker hintText="" defaultDate={this.state.endDate} onChange={this.handleChangeEndDate} textFieldStyle={styles.customDate} /> },
+      { id: 3, label: <p>Locations: </p>, setting: <LocationFilter /> },
+    ];
+    return (
+      <div>
+        <div style={{display: 'flex'}}>
+          <h2> Filter </h2>
+          <span style={{marginLeft: 10, marginTop: 25,}}>(to show only properties that match criteria)</span>
+        </div>
+        {
+          Date.map(date =>
+            <div key={date.id} style={styles.date}>
+              {date.label}
+              {date.setting}
+            </div>,
+          )
+        }
+        <div style={styles.applyContainer}>
+          <Checkbox
+            label="List only properties with all dates available"
+            style={styles.checkbox}
+          />
+          <RaisedButton
+            label="Apply filter"
+            labelStyle={styles.textStyles}
+            backgroundColor={colors.btnColors}
+            style={styles.customStyle}
+          />
+        </div>
       </div>
-      {
-        Date.map(date =>
-          <div key={date.id} style={styles.date}>
-            {date.label}
-            {date.setting}
-          </div>,
-        )
-      }
-      <div style={styles.applyContainer}>
-        <Checkbox
-          label="List only properties with all dates available"
-          style={styles.checkbox}
-        />
-        <RaisedButton
-          label="Apply filter"
-          labelStyle={styles.textStyles}
-          backgroundColor={colors.btnColors}
-          style={styles.customStyle}
-        />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Filter;
