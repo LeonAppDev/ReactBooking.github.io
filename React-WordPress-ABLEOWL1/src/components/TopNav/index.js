a8024beb0ecfcb2b1e594e7961869a594cb4c24f
Liang Ren <liangren64@gmail.com>
2017-06-27 00:54:52 -0300

    Add sample component

--- React-WordPress-ABLEOWL1/src/components/TopNav/index.js
+++ React-WordPress-ABLEOWL1/src/components/TopNav/index.js
@@ -8,6 +8,7 @@ import { getPosts } from 'reducers/postsEndpoint/actions';
 import { pagesInfoSelector } from 'reducers/pagesEndPoint/selectors';
 import { postsInfoSelector } from 'reducers/postsEndpoint/selectors';
 // import styleTopNav from './style';
+import Sample from './Sample';
 
 const mapStateToProps = state => ({
   pages: pagesInfoSelector(state),
@@ -19,7 +20,13 @@ const mapDispatchToProps = dispatch => ({
   getPages: () => dispatch(getPages()),
 });
 
-
+function Sample1() {
+  return (
+    <div>
+      <h1> this is sample1 component </h1>
+    </div>
+  );
+}
 
 export class TopNav extends Component {
   static propTypes = {
@@ -62,12 +69,14 @@ handleClick(id)
 
       return (
         <header>
+          <Sample />  /////// used another component ////////////
           {
 
             pages.map(page =>
               <Link key={page.get('id')} to={`/${page.get('slug')}`} onClick={()=>this.handleClick(page.get('id'))} >{page.getIn(['title','rendered'])}</Link>
             )
           }
+          <Sample1 />
         </header>
       );
   }
